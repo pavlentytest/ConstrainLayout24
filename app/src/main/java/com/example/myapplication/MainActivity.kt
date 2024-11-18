f09bdcc94dc6606dae0023219aba4d370c1b30fe
Pavel Stepanov <impetus@yandex.ru>
2024-11-18 15:03:44 +0300

    Add simple counter

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,14 +1,33 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var button: Button
+    lateinit var tv: TextView
+    var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.departButton)
+        tv = findViewById(R.id.spaceStationLabel)
+
+        button.setOnClickListener {
+            counter++
+            tv.text = counter.toString()
+        }
     }
 }
+
+
+
+
+
+
+
